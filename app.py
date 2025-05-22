e0cc1a59a801fb24270e40b476672ec5f4c05675
Mudi Li <obj_barrier@163.com>
2025-05-22 02:29:11 -0500

    Change host to 0.0.0.0

--- app.py
+++ app.py
@@ -254,4 +254,4 @@ def api_end_shopping_session(session_id):
 
 if __name__ == '__main__':
     print("Starting Flask server...")
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
